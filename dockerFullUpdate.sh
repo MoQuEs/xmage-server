e48de6009632bf2c3edf7dcfc7b8fa0e947a4b7d
Sebastian <moques93@gmail.com>
2018-11-16 21:55:15 +0100

    Fix update script

--- dockerFullUpdate.sh
+++ dockerFullUpdate.sh
@@ -2,13 +2,7 @@
 
 CURRENT_DIR=$(cd $(dirname "$0") && pwd -P)
 
-
 git reset --hard origin/master
 git pull
 
-
-docker stop moques_docker-xmage-alpine
-docker rm moques_docker-xmage-alpine
-docker rmi moques/docker-xmage-alpine
-
-docker-compose -f $CURRENT_DIR"/docker-compose.yml" up --remove-orphans --force-recreate --build -d
+bash $CURRENT_DIR"/dockerStartDocker.sh"
